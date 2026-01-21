c806f2d4457143359cc1f09ebd815c4a53b4b272
Sam.Richards@taurich.org <Sam.Richards@taurich.org>
2026-01-21 23:08:06 +0000

    Update timingtest.py

--- timingtest.py
+++ timingtest.py
@@ -69,6 +69,7 @@ def run_cmd(cmd, log_file=None):
 
 
 source_exrs = "/Users/sam/git/EncodingGuidelines/enctests/sources/hdr_sources/sparks/SPARKS_ACES_#.exr"
+#source_exrs = "test_frames/frame.#.exr"
 testoutputdir = "./outputtimingtest"
 logfile = "timing_test_log.txt"
 
@@ -78,9 +79,12 @@ if not os.path.exists(testoutputdir):
 t = time.time()
 cmd = f"oiiotool -v --framepadding 5 --parallel-frames --frames 6100-6299 {source_exrs} --iscolorspace \"ACEScg\" --ociodisplay \"Rec.2100-PQ - Display\" \"ACES 1.1 - HDR Video (1000 nits & Rec.2020 lim)\" -d uint16 -o {testoutputdir}/sparks2_pq1000.#.png"
 run_cmd(cmd)
+oiiotool_elapsed = time.time() - t
 
+t1 = time.time()
 ffmpegcmd = "ffmpeg -y -framerate 24 -start_number 6100 -i {}/sparks2_pq1000.%05d.png -c:v prores_ks -pix_fmt yuv422p10le -profile:v 3 -vendor apl0 -vf \"scale=in_range=full:in_color_matrix=bt2020:out_range=tv:out_color_matrix=bt2020\" -color_range tv -color_trc smpte2084 -color_primaries bt2020 -colorspace bt2020nc {}/sparks2_pq1000_prores10bit.mov".format(testoutputdir, testoutputdir)
 run_cmd(ffmpegcmd)
+basic_ffmpeg_elapsed = time.time() - t1
 
 elapsed = time.time() - t
 
@@ -93,5 +97,61 @@ run_cmd(ffmpegcmd)
 
 ffmpeg_elapsed = time.time() - t
 
+
+
+start_frame = 6100
+end_frame = 6299
+
+def make_path(pattern, frame):
+    # find contiguous run of '#'
+    import re
+    m = re.search(r"(#+)", pattern)
+    if m:
+        pad = 5
+        num = str(frame).zfill(pad)
+        return pattern[:m.start()] + num + pattern[m.end():]
+    # fallback: simple replace single '#'
+    return pattern.replace("#", str(frame))
+
+
+with open("odd.txt", "w") as oddf, open("even.txt", "w") as evenf:
+    for f in range(start_frame, end_frame + 1):
+        path = make_path(source_exrs, f)
+        line = f"file '{path}'\n"
+        if f % 2 == 0:
+            evenf.write(line)
+        else:
+            oddf.write(line)
+
+
+ffmpegcmd_parallel = """ffmpeg -y \
+-f concat -safe 0 -r 12 -i odd.txt \
+-f concat -safe 0 -r 12 -i even.txt \
+-filter_complex_threads 2 \
+-threads 2 \
+-filter_complex \"\
+    [0:v] ocio=input=ACEScg:display=Rec.2100-PQ - Display:view=ACES 1.1 - HDR Video (1000 nits & Rec.2020 lim):format=rgb48, 
+          scale=in_range=full:in_color_matrix=bt2020:out_range=tv:out_color_matrix=bt2020, drawtext=fontfile='/System/Library/Fonts/Monaco.ttf':text=\'ODD %{{eif\\:6100+n*2\\:d}}\':fontsize=100:fontcolor=white:x=50:y=50,
+          format=yuv422p10le [odd_processed];
+    [1:v] ocio=input=ACEScg:display=Rec.2100-PQ - Display:view=ACES 1.1 - HDR Video (1000 nits & Rec.2020 lim):format=rgb48, 
+          scale=in_range=full:in_color_matrix=bt2020:out_range=tv:out_color_matrix=bt2020, 
+          setpts=PTS+0.5, drawtext=fontfile='/System/Library/Fonts/Monaco.ttf':text=\'EVEN %{{eif\\:6100+n*2+1\\:d}}\':fontsize=100:fontcolor=white:x=50:y=50,
+          format=yuv422p10le [even_processed];
+    [even_processed][odd_processed] interleave
+\" \
+-r 24 \
+-c:v prores_ks -profile:v 3 -vendor apl0 \
+-color_range tv -color_trc smpte2084 -color_primaries bt2020 -colorspace bt2020nc \
+{testoutputdir}/sparks2_pq1000_prores10bit_ffmpegparallel.mov""".format(testoutputdir=testoutputdir)
+
+t = time.time()
+print(ffmpegcmd_parallel)
+#ffmpegcmd = f"ffmpeg -y -framerate 24 -start_number 6100 -i {ffmpeg_source} -c:v prores_ks -pix_fmt yuv422p10le -profile:v 3 -vendor apl0 -threads 0 -filter_threads 0 \-vf \"ocio=input=ACEScg:display=Rec.2100-PQ - Display:view=ACES 1.1 - HDR Video (1000 nits & Rec.2020 lim):format=rgb48,scale=in_range=full:in_color_matrix=bt2020:out_range=tv:out_color_matrix=bt2020\" -color_range tv -color_trc smpte2084 -color_primaries bt2020 -colorspace bt2020nc {testoutputdir}/sparks2_pq1000_prores10bit_ffmpeg.mov"
+#run_cmd(ffmpegcmd_parallel)
+#parallel_ffmpeg_elapsed = time.time() - t
+
+print(f"Elapsed time for oiiotool: {oiiotool_elapsed} seconds")
+print(f"Elapsed time for basic ffmpeg: {basic_ffmpeg_elapsed} seconds")
 print(f"Elapsed time for oiiotool + ffmpeg: {elapsed} seconds")
 print(f"Elapsed time for ffmpeg only: {ffmpeg_elapsed} seconds")
+#print(f"Elapsed time for parallel ffmpeg only: {parallel_ffmpeg_elapsed} seconds")
